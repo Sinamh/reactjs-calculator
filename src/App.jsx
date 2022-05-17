28c7941bdda7ebb9c0448041395110bab5fb5906
Sina Mohammadian <34884156+Sinamh@users.noreply.github.com>
2022-05-17 20:23:17 +0430

    Start project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Calc from "./components/Calc";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <Calc />
+      <footer className="App-footer"></footer>
+    </div>
+  );
+}
+
+export default App;
